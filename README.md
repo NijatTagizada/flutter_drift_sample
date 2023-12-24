04d2af9b292f1bae0c59b7e12556f40024bf4fe7
NijatTagizada <tagizada.nicat@gmail.com>
2023-12-25 00:33:55 +0400

    init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_drift_sample
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_drift_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_drift_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
